4c96ced06203f59cbf8bfa8591e57e663fc645b5
simonyang <simonyang777@qq.com>
2024-08-03 08:25:18 +0800

    fix: 修复复杂场景下 Back 不生效问题

--- src/background.ts
+++ src/background.ts
@@ -1,40 +1,57 @@
 chrome.action.onClicked.addListener(start);
 
+const tabIdWindowIdMap: Record<number, number> = {}
+let listener: any = undefined
+
 async function start() {
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
   const currentTab = tabs[0];
-  const lastWindowId = currentTab.windowId
+  tabIdWindowIdMap[currentTab.id] = currentTab.windowId
 
   await chrome.windows.create({
     type: 'popup',
     tabId: currentTab.id
   })
 
-  const listener = async (message) => {
-    console.log('received message:', message)
-    if (message !== 'reset') { return }
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-    if (tab.id !== currentTab.id) { return }
-    chrome.tabs.move(tab.id!, { windowId: lastWindowId, index: -1 })
-    chrome.tabs.update(
-      currentTab.id!,
-      { active: true }
-    )
-    chrome.runtime.onMessage.removeListener(listener)
+  if (!listener) {
+    listener = async (message) => {
+      console.log('received message:', message)
+
+      if (message !== 'reset') { return }
+
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+      if (!tabIdWindowIdMap[tab.id]) { return }
+
+      chrome.tabs.move(tab.id!, { windowId: tabIdWindowIdMap[tab.id], index: -1 })
+      chrome.tabs.update(
+        tab.id!,
+        { active: true }
+      )
+
+      delete tabIdWindowIdMap[tab.id]
+
+      if (Object.keys(tabIdWindowIdMap).length === 0) {
+        chrome.runtime.onMessage.removeListener(listener)
+        listener = undefined
+      }
+    }
+    chrome.runtime.onMessage.addListener(listener)
   }
-  chrome.runtime.onMessage.addListener(listener)
 }
 
 chrome.tabs.onAttached.addListener(async (tabId, attachInfo) => {
-  const window = await chrome.windows.get(attachInfo.newWindowId)
-  if (window.type !== 'popup') { return }
+  if (!tabIdWindowIdMap[tabId]) { return }
   attachElementAndStyle(tabId)
 })
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  const window = await chrome.windows.get(tab.windowId)
-  if (window.type !== 'popup') { return }
+  if (!tabIdWindowIdMap[tabId]) { return }
   attachElementAndStyle(tabId)
+})
+
+chrome.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
+  if (!tabIdWindowIdMap[tabId]) { return }
+  delete tabIdWindowIdMap[tabId]
 })
 
 function attachElementAndStyle(tabId: number) {
